59b0495119dd50f6eb1b9ff547967fe001207cfb
222abdulaziz <106725986+222abdulaziz@users.noreply.github.com>
2022-07-16 18:43:08 +0500

    python dars

---
+++ dars1.py
@@ -0,0 +1,62 @@
+
+# name = "John"
+# print(name)
+############
+# import keyword
+# print(keyword.kwlist)
+############
+# 1-mosol
+# p = int("4")
+# print(f"perimetr {p * 4}")
+
+# 2-mosol
+# y =int("3")
+# print(f"s:{y**2}")
+
+# 3-misol
+# y = int("2")
+# print(f"s:{y**2} va p:{y*4}")
+
+# 4-misol
+# p = 3.14
+# l = int("3")
+# print(f"d:{p*l}")
+
+# 5-misol
+# x = int("2")
+# print(f"{6}")
+
+# 6-misol
+# a = int("2")
+# b = int("2")
+# c= int("2")
+# print(f"S:{(a*b + b*c+ a*c)}")
+
+# 7
+# r = int("2")
+# print(f"L:{2*pi*r} S:{pi**2")
+
+# 8
+a = int("2")
+b =int("4")
+print(f"{(a+b)/2}")
+
+# 12
+# print(f"C:{(a**2 + b**2)**1/2}")
+
+# 13
+
+
+# 14
+# y = 5
+# print(f"Y:{2*pi * r} S:{pi * r **2} ")
+
+# 15
+# a = 5
+# print(f"D:{r*2} L:{2* pi * (10/pi)**2}")
+
+# 18
+# a = 2
+# b=16
+# c=5
+# print(f"AC:{c-a} CB:{b-c}")
